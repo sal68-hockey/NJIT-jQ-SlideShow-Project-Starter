5372f1c6515df6debf6408135394ffe9f26e02f9
sal68-hockey <526smontecalvo@frhsd.com>
2025-11-19 09:40:08 -0500

    fish knee

--- gallery.js
+++ gallery.js
@@ -1,79 +1,62 @@
-let mCurrentIndex = 0 // Tracks the current image index
-let mImages = [] // Array to hold GalleryImage objects
-const mUrl = 'https://your-json-url.com' // Replace with actual JSON URL
-const mWaitTime = 5000 // Timer interval in milliseconds
-let mTimer // Store the timer reference
+let mCurrentIndex = 0;
+let mImages = [];
+const mUrl = "images.json";
+const mWaitTime = 5000;
+let mTimer;
 
 $(document).ready(() => {
-  $('.details').hide() // Hide details initially
+    $('.details').hide();
 
-  // Start the timer for the slideshow
-  startTimer()
+    fetchJSON();
+    startTimer();
 
-  // Toggle more info section when the indicator is clicked
-  $('.moreIndicator').click(function () {
-    $(this).toggleClass('rot90 rot270')
-    $('.details').slideToggle('slow')
-  })
+    $('.moreIndicator').click(function () {
+        $(this).toggleClass('rot90 rot270');
+        $('.details').slideToggle('slow');
+    });
 
-  
-  $('#nextPhoto').click(() => {
-    showNextPhoto()
-  })
+    $('#nextPhoto').click(() => { showNextPhoto(); });
+    $('#prevPhoto').click(() => { showPrevPhoto(); });
+});
 
-  
-  $('#prevPhoto').click(() => {
-    showPrevPhoto()
-  })
-
-  
-  fetchJSON()
-})
-
-
-function fetchJSON () {
-  $.ajax({
-    url: mUrl,
-    dataType: 'json',
-    success: function (data) {
-      mImages = data.images 
-      swapPhoto()
-    },
-    error: function (xhr, status, error) {
-      console.error('Error loading JSON:', error)
-    }
-  })
+function fetchJSON() {
+    $.ajax({
+        url: mUrl,
+        dataType: 'json',
+        success: function (data) {
+            mImages = data.images;
+            swapPhoto();
+        },
+        error: function (xhr, status, error) {
+            console.error('Error loading JSON:', error);
+        }
+    });
 }
 
-function swapPhoto () {
-  if (mImages.length === 0) return
-
-  const currentImage = mImages[mCurrentIndex]
-  $('#photo').attr('src', currentImage.imgPath)
-  $('.location').text('Location: ' + currentImage.imgLocation)
-  $('.description').text('Description: ' + currentImage.description)
-  $('.date').text('Date: ' + currentImage.date)
+function swapPhoto() {
+    if (mImages.length === 0) return;
+    const currentImage = mImages[mCurrentIndex];
+    $('#photo').fadeOut(300, function () {
+        $(this).attr('src', currentImage.imgPath).fadeIn(300);
+    });
+    $('#imgfish').text(currentImage.imgfish);
+    $('#description').text(currentImage.description);
+    $('#avgsize').text(currentImage.avgsize);
 }
 
-
-function showNextPhoto () {
-  mCurrentIndex++
-  if (mCurrentIndex >= mImages.length) {
-    mCurrentIndex = 0
-  }
-  swapPhoto()
+function showNextPhoto() {
+    if (mImages.length === 0) return;
+    mCurrentIndex = (mCurrentIndex + 1) % mImages.length;
+    swapPhoto();
 }
 
-function showPrevPhoto () {
-  mCurrentIndex--
-  if (mCurrentIndex < 0) {
-    mCurrentIndex = mImages.length - 1
-  }
-  swapPhoto()
+function showPrevPhoto() {
+    if (mImages.length === 0) return;
+    mCurrentIndex = (mCurrentIndex - 1 + mImages.length) % mImages.length;
+    swapPhoto();
 }
 
-
-function startTimer () {
-  clearInterval(mTimer) 
-  mTimer = setInterval(showNextPhoto, mWaitTime)
+function startTimer() {
+    clearInterval(mTimer);
+    mTimer = setInterval(showNextPhoto, mWaitTime);
 }
